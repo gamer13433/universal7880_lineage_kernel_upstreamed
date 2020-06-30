a066a3d242a6fa6ab05876fbf4feaa5d8dd12853
gamer13433 <saintpi70@gmail.com>
2020-07-01 01:44:38 +0500

    finaly update to 3.18.140

--- init/main.c
+++ init/main.c
@@ -601,6 +601,8 @@ asmlinkage __visible void __init start_kernel(void)
 	page_alloc_init();
 
 	pr_notice("Kernel command line: %s\n", boot_command_line);
+	/* parameters may set static keys */
+	jump_label_init();
 	parse_early_param();
 	after_dashes = parse_args("Booting kernel",
 				  static_command_line, __start___param,
@@ -621,8 +623,6 @@ asmlinkage __visible void __init start_kernel(void)
 	vmm_init();
 #endif //CONFIG_KNOX_KAP
 #endif //CONFIG_TIMA_RKP
-
-	jump_label_init();
 
 	/*
 	 * These use large bootmem allocations and must precede
